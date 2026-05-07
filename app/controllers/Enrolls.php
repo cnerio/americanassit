32ff52a82ee8eab586ed8ef74065d2972d412053
cnerio <xneriox@gmail.com>
2026-05-07 11:24:44 -0500

    Update Enrolls.php

--- app/controllers/Enrolls.php
+++ app/controllers/Enrolls.php
@@ -1985,7 +1985,7 @@ public function old_check()
                  "type_doc"=>"Consent"
                ];
                $this->enrollModel->saveData($fileData,'lifeline_documents');
-              $ConsentFileResult = $this->APIService->sendDocuments($customerId,$orderId,"Consent",$this->enrollModel);
+              $ConsentFileResult = $this->APIService->sendDocuments($customerId,$orderId,"Consent",$this->enrollModel,'AMBT');
               print_r($ConsentFileResult);
               $processData['process_status']=$ConsentFileResult['msg'];
               $this->enrollModel->updateData($processData,'lifeline_records');
