6127f4ea9993a9c5777c719e2d6edcc47b1b2b7e
cnerio <xneriox@gmail.com>
2026-05-19 08:13:30 -0500

    Comment out debug print_r in Enrolls controller
    Disable a debug print of the Consent API response in app/controllers/Enrolls.php (old_check()). The print_r($ConsentFileResult) call was commented out to prevent debug output from being sent to the response stream and to avoid leaking potentially sensitive data or cluttering logs; subsequent processing (updating process_status from $ConsentFileResult['msg']) is unchanged.

--- app/controllers/Enrolls.php
+++ app/controllers/Enrolls.php
@@ -1986,7 +1986,7 @@ public function old_check()
                ];
                $this->enrollModel->saveData($fileData,'lifeline_documents');
               $ConsentFileResult = $this->APIService->sendDocuments($customerId,$orderId,"Consent",$this->enrollModel,'AMBT');
-              print_r($ConsentFileResult);
+              //print_r($ConsentFileResult);
               $processData['process_status']=$ConsentFileResult['msg'];
               $this->enrollModel->updateData($processData,'lifeline_records');
                 $result=[
